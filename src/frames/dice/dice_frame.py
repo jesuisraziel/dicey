e050894e58669c7081e46cbee483171d7c57b29a
Jesus Gonzalez <jrazielgsosa@gmail.com>
2023-10-13 07:10:04 -0400

    Added spacing

--- src/frames/dice/dice_frame.py
+++ src/frames/dice/dice_frame.py
@@ -12,12 +12,16 @@ class DiceFrame(ttk.Frame):
         d2 = DiceButton(self, label="d2", options=PositionOptions(row=0, col=0))
         d4 = DiceButton(self, label="d2", options=PositionOptions(row=0, col=1))
         d6 = DiceButton(self, label="d2", options=PositionOptions(row=0, col=2))
+
         d8 = DiceButton(self, label="d2", options=PositionOptions(row=1, col=0))
         d10 = DiceButton(self, label="d2", options=PositionOptions(row=1, col=1))
         d12 = DiceButton(self, label="d2", options=PositionOptions(row=1, col=2))
+
         d20 = DiceButton(self, label="d2", options=PositionOptions(row=2, col=0))
         d100 = DiceButton(self, label="d2", options=PositionOptions(row=2, col=1))
         dX = DiceButton(self, label="d2", options=PositionOptions(row=2, col=2))
 
+        self.columnconfigure(index=[0,1,2], pad="12px")
+        self.rowconfigure(index=[0,1,2], pad="12px")
         self.grid(column=col, row=row, columnspan=3, rowspan=3)
         
